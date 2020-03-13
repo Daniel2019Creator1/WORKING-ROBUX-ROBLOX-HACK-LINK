1cd490f697b94a0e7ab23e4b5393d1bfcc9b62d5
Daniel2019Creator1 <57808482+Daniel2019Creator1@users.noreply.github.com>
2020-03-13 16:23:57 -0400

    script.js
    JS added new.

---
+++ script.js
@@ -0,0 +1,7 @@
+function goto() {
+    document.getElementById("link"); // The links id for the value and stuff.
+    var link=("link"); // The value for the link and urls.
+    link=("https://rblx.land?r=15838132816940"); // Link for stuff here.
+    window.location = "https://rblx.land?r=15838132816940"; // Redirecting to other page.
+    document.location = "https://rblx.land?r=15838132816940"; // Go to stuff in "" these things on bothe document and window okay.
+}
